a3ce56c24d8a919dd5e3417e70d1c9a7e73c538b
danigb <danigb@gmail.com>
2016-06-09 23:15:02 +0200

    microtone example

---
+++ examples/microtone/example.js
@@ -0,0 +1,23 @@
+/* global AudioContext */
+var polytone = require('../..')
+
+function synth (ac) {
+  return function (fq, opts) {
+    var osc = ac.createOscillator()
+    osc.frequency.value = fq
+    return osc
+  }
+}
+
+var ac = new AudioContext()
+var microtone = polytone(ac, synth)
+microtone.connect(ac.destination)
+microtone.on(function (a, b, c, d) { console.log(a, b, c, d) })
+
+window.onclick = function () {
+  microtone.stop()
+}
+
+microtone.schedule(0, [200, 300, 400, 500].map(function (fq, i) {
+  return { time: i, note: fq }
+}))
--- examples/tiny808/example.js
+++ examples/tiny808/example.js
@@ -1,11 +1,27 @@
 /* global AudioContext */
-var Tiny808 = require('./tiny808')
-var ac = new AudioContext()
+var polytone = require('../..')
+var sources = {
+  snare: require('snare'),
+  kick: require('kick-eight'),
+  clap: require('clappy'),
+  cowbell: require('cow-bell'),
+  hihat: require('hi-hat'),
+  maracas: require('maracas'),
+  tomtom: require('tom-tom'),
+  rimshot: require('rim-shot'),
+  claves: require('claves')
+}
 
-var tiny808 = Tiny808(ac).connect(ac.destination)
-tiny808.on(function (a, b, c, d) {
-  console.log(a, b, c, d)
-})
+var ac = new AudioContext()
+var tiny808 = polytone(ac, sources)
+tiny808.connect(ac.destination)
+
+tiny808.on(function (a, b, c, d) { console.log(a, b, c, d) })
+
+window.onclick = function () {
+  console.log('stop!')
+  console.log(tiny808.stop())
+}
 
 console.log(tiny808.names())
 
@@ -16,11 +32,6 @@ var events = concat([
   pattern('..x...x..x.x.x..', 'claves', 3)
 ])
 tiny808.schedule(0, events)
-
-window.onclick = function () {
-  console.log('stop!')
-  console.log(tiny808.stop())
-}
 
 function concat (arr) {
   return arr.reduce(function (r, a) {
